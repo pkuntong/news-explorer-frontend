9e141c3c266bd564c5911abc815ade157c9fc81f
Your Name <you@example.com>
2025-03-06 12:05:40 -0500

    initial updating about.jsx, css

---
+++ src/components/About/About.jsx
@@ -0,0 +1,30 @@
+import './About.css';
+import authorProfile from '../../assets/author-image.jpg';
+function About() {
+  return (
+    <section className='about'>
+      <div className='about__container'>
+        <div className='about__left'>
+          <img
+            src={authorProfile}
+            alt='author_profile'
+            className='about__profile'
+          />
+        </div>
+        <div className='about__right'>
+          <h2 className='about__header'>About the author</h2>
+          <p className='about__text'>
+            Hi! My name is Pau Kuntong and I am a Full-Stack
+            Software Engineer based in Washington DC area.
+            <br />
+            <br />
+            It was incredible experience time at TripleTen!
+            I gained so many valuable skills in JavaScript, React, and Backend Development with TripleTen.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
