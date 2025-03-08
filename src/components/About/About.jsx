546c119ae719cd9e30a124e2334998e25c065893
Your Name <you@example.com>
2025-03-08 16:21:01 -0500

    i hope last updating

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,12 +14,11 @@ function About() {
         <div className='about__right'>
           <h2 className='about__header'>About the author</h2>
           <p className='about__text'>
-            Hi! My name is Pau Kuntong and I am a Full-Stack
-            Software Engineer based in Washington DC area.
+            Hello! I'm Pau Kuntong, a Full-Stack Software Engineer based in the Washington, DC area.
             <br />
             <br />
-            It was incredible experience time at TripleTen!
-            I gained so many valuable skills in JavaScript, React, and Backend Development with TripleTen.
+            My time at TripleTen was truly transformative, allowing me to develop strong skills in JavaScript, React, and Backend Development.
+            It was an invaluable experience that helped shape my expertise in the field.
           </p>
         </div>
       </div>
